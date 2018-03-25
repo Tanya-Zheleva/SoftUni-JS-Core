88c3ee52f7511fbfc3b8173f71382971082f7c83
Tanya Zheleva <tanya.zhelevaa@gmail.com>
2018-03-25 17:31:10 +0300

    Added JS Apps module

---
+++ JS Apps/AJAX/Phonebook/app.js
@@ -0,0 +1,101 @@
+$(() => {
+    const baseUrl = 'https://phonebook-1e69f.firebaseio.com/phonebook';
+    const list = $('#list');
+    $('#btnCreate').on('click', create);
+    loadContacts();
+
+    function loadContacts() {
+        let req = {
+            url: baseUrl + '.json',
+            success: displayContacts
+        };
+
+        $.ajax(req);
+    }
+
+    function displayContacts(data) {
+        list.empty();
+        for (let contact in data) {
+            let html = $(`<li><span>${data[contact].name}: ${data[contact].phone}</span> </li>`);
+            html.append($(`<button>Delete</button>`).on('click', () => deleteContact(contact)));
+            list.append(html);
+        }
+    }
+
+    function create() {
+        let name = $('#createName').val();
+        let phone = $('#createPhone').val();
+
+        if (name.length === 0) {
+            notify('Name cannot be empty', 'error');
+            return;
+        }
+
+        if (phone.length === 0) {
+            notify('Phone cannot be empty', 'error');
+            return;
+        }
+
+        $('#btnCreate').prop('disabled', true);
+
+        let contact = {
+            name,
+            phone
+        };
+
+        let req = {
+            url: baseUrl + '.json',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(contact),
+            success: () => {
+                $('#createName').val('');
+                $('#createPhone').val('');
+                notify('Created', 'success');
+                loadContacts();
+            },
+            error: displayError,
+            complete: () => $('#btnCreate').prop('disabled', false)
+        };
+
+        $.ajax(req);
+    }
+
+    function displayError(err) {
+        notify(`Error: ${err.statusText}`, 'error');
+    }
+
+    function notify(message, type) {
+        let toast = document.getElementById('notification');
+        toast.textContent = message;
+        toast.style.display = 'block';
+
+        switch (type) {
+            case 'error':
+                toast.style.backgroundColor = '#991111';
+                break;
+            case 'success':
+                toast.style.backgroundColor = '#119911';
+                break;
+            case 'info':
+                toast.style.backgroundColor = '#111199';
+                break;
+        }
+
+        setTimeout(() => toast.style.display = 'none', 2000);
+    }
+
+    function deleteContact(contact) {
+        let req = {
+            url: `${baseUrl}/${contact}.json`,
+            method: 'DELETE',
+            success: () => {
+                notify('Deleted', 'info');
+                loadContacts();
+            },
+            error: displayError
+        };
+
+        $.ajax(req);
+    }
+});
